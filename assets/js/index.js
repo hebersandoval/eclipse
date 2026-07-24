eb0c9bc1187c959c6c4548fbf3b89cf9270f03f2
Heber Sandoval <he3ers@gmail.com>
2026-07-23 22:45:58 -0500

    Made more foundational adjustment to style of design.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,18 @@
-console.log('Heelooooo there!');
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+            });
+        }
+    });
+});
 
+// Collapsible functionality
 const collapsibles = document.querySelectorAll('.collapsible');
 
 collapsibles.forEach((item) => {
